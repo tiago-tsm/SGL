c7874dce49c27c25aea886e64259d6a059bd0857
Tiago Soares Miranda <tiago.miranda@tsmdebug.com.br>
2024-08-25 14:07:05 -0300

    requirements.txt

--- app/settings.py
+++ app/settings.py
@@ -85,14 +85,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sge',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT':  '5432',
-        
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
