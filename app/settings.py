a152013441440822ee78284ebfd13aa887f880ea
Tiago Soares Miranda <tiago.miranda@tsmdebug.com.br>
2024-08-24 23:56:13 -0300

    add BD postgreSQL

--- app/settings.py
+++ app/settings.py
@@ -85,8 +85,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sge',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT':  '5432',
+        
+        
     }
 }
 
