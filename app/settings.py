ca4fee51ab50332a86313d23997d5edcb6003d73
Tiago Soares Miranda <tiago.miranda@tsmdebug.com.br>
2024-08-25 15:28:07 -0300

    requirements.txt

--- app/settings.py
+++ app/settings.py
@@ -60,10 +60,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'app.urls'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
