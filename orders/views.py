a06c63b790b4ae494230f3df72d19e86fce3cc9a
Tiago Soares Miranda <tiago.miranda@tsmdebug.com.br>
2024-08-25 19:52:26 -0300

    editando Header

--- orders/views.py
+++ orders/views.py
@@ -17,7 +17,7 @@ class OrdersCreateView(LoginRequiredMixin, CreateView):
 
 class OrdersUpdateView(LoginRequiredMixin, UpdateView):
     model = Orders
-    template_name = 'orders/order_.update.html'
+    template_name = 'orders_update.html'
     form_class = OrdersForm
     success_url = reverse_lazy('orders_list')  # Nome da URL para redirecionar após a atualização
 
