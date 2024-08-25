53362bf0647a1a2ce5cb5de8d556fd5980d6096d
Tiago Soares Miranda <tiago.miranda@tsmdebug.com.br>
2024-08-25 14:32:55 -0300

    requirements.txt

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u9cro&cs(@nq-7qm1-yl)4mvxzu@x=_vu%^$nr2!d2e1zone-r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
